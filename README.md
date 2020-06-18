94e52b8f5cc284ec1161f32dca0b95e1fcb0b61a
Juan Fumero <jjfumero@gmail.com>
2020-06-18 16:00:03 +0200

    Initial commit - TornadoVM installers

---
+++ README.md
@@ -0,0 +1,75 @@
+## TornadoVM Installer
+
+Scripts to install [TornadoVM](https://github.com/beehive-lab/TornadoVM/)
+
+​
+**Pre-requisites**
+
+* Maven Version 3
+* OpenCL: GPUs and CPUs >= 1.2, FPGAs >= 1.0
+* GCC or clang/LLVM (GCC >= 5.5)
+* Python 2.7 (>= 2.7.5)
+* JDK 8 >= 1.8.0_141
+
+
+### How to use? 
+
+The scripts provided in this repository will compile/download OpenJDK, `cmake` and it will build TornadoVM. 
+
+
+To use TornadoVM with OpenJDK 8:
+
+
+```bash
+./install-tornadovm-jdk8.sh
+# and follow instructions
+```
+
+
+To build TornadoVM with GraalVM and JDK 11:
+
+
+```bash
+./install-tornadovm-graalvm11.sh
+# and follow instructions
+```
+
+After the installation, the scripts create a directory with the TornadoVM SDK. The directory also includes a source file with all variables needed to start using TornadoVM. 
+
+
+Example:
+```bash
+$ source TornadoVM-JDK8/TornadoVM/source.sh
+$ tornado --version
+version=0.6
+branch=master
+commit=4695f5c
+
+$ tornado --devices
+
+Number of Tornado drivers: 1
+Total number of devices  : 3
+Tornado device=0:0
+	NVIDIA CUDA -- GeForce GTX 1050
+		Global Memory Size: 3.9 GB
+		Local Memory Size: 48.0 KB
+		Workgroup Dimensions: 3
+		Max WorkGroup Configuration: [1024, 1024, 64]
+		Device OpenCL C version: OpenCL C 1.2
+
+Tornado device=0:1
+	Intel(R) OpenCL -- Intel(R) Core(TM) i7-7700HQ CPU @ 2.80GHz
+		Global Memory Size: 31.0 GB
+		Local Memory Size: 32.0 KB
+		Workgroup Dimensions: 3
+		Max WorkGroup Configuration: [8192, 8192, 8192]
+		Device OpenCL C version: OpenCL C 1.2
+
+Tornado device=0:2
+	Intel(R) OpenCL HD Graphics -- Intel(R) Gen9 HD Graphics NEO
+		Global Memory Size: 24.8 GB
+		Local Memory Size: 64.0 KB
+		Workgroup Dimensions: 3
+		Max WorkGroup Configuration: [256, 256, 256]
+		Device OpenCL C version: OpenCL C 2.0
+```
---
+++ install-tornadovm-jdk8.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+currentver="$(gcc -dumpversion)"
+requiredver="5.5.0"
+if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ]; then 
+        echo "GCC Vesion: OK"
+else
+        echo "Less than 5.5.0"
+		exit
+fi
+
+
+if [ -z "$JAVA_HOME" ];
+then
+	echo "JAVA_HOME is not set. Use OpenJDK 8 >= 141 <= 1.9"
+	echo "\t You can use \`ls -l /etc/alternatives/java\` to get the PATHs"
+ 	exit 0
+else
+ 	echo "JDK Version: OK"
+fi
+
+# 1) Download OpenJDK with JVMCI support
+mkdir -p TornadoVM-JDK8
+
+cd TornadoVM-JDK8
+JDK_BASE=`pwd`
+git clone --depth 1 https://github.com/beehive-lab/mx
+export PATH=`pwd`/mx:$PATH
+git clone --depth 1 https://github.com/beehive-lab/graal-jvmci-8
+cd graal-jvmci-8
+mx build -p
+DIR=`ls `pwd`openjdk*`
+OPENJDK=`ls | grep openjdk`
+export JAVA_HOME=`pwd`/$OPENJDK/linux-amd64/product/
+cd -
+
+# 2) Download CMAKE
+wget https://github.com/Kitware/CMake/releases/download/v3.18.0-rc2/cmake-3.18.0-rc2-Linux-x86_64.tar.gz
+tar xzf cmake-3.18.0-rc2-Linux-x86_64.tar.gz
+export PATH=`pwd`/cmake-3.18.0-rc2-Linux-x86_64/bin:$PATH
+export CMAKE_ROOT=`pwd`/cmake-3.18.0-rc2-Linux-x86_64/
+
+# 3) Download TornadoVM
+git clone --depth 1 https://github.com/beehive-lab/TornadoVM
+cd TornadoVM
+export PATH=$PWD/bin/bin:$PATH
+export TORNADO_SDK=$PWD/bin/sdk
+make 
+
+echo -e "To use TornadoVM, export the following variables:\n"
+
+echo "Creating Source File ....... " 
+	echo "export JAVA_HOME=$JAVA_HOME" > source.sh
+	echo "export PATH=$PWD/bin/bin:\$PATH" >> source.sh
+	echo "export TORNADO_SDK=$PWD/bin/sdk" >> source.sh
+	echo "export CMAKE_ROOT=$PWD/cmake-3.18.0-rc2-Linux-x86_64/" >> source.sh
+	echo "export TORNADO_ROOT=$PWD " >> source.sh
+echo "........................... [OK]"
+
+
+echo "To run TornadoVM, run \`. TornadoVM-JDK8/TornadoVM/source.sh\`"
+
+
